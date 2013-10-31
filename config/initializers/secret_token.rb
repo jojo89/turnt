5f5d15c489bbfd9ab89311f67b2a7bc74f886f3b
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-31 17:31:34 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Turnt::Application.config.secret_key_base = '595c9a7acb0813e1c85e80436acc042c612827a12fe882ce6905ae558c3864ac5bfdfe94f04fadb2558ae2d92001f1b7e48ea8caae026c0b798f1c05408d4267'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Turnt::Application.config.session_store :cookie_store, key: '_Turnt_session'
