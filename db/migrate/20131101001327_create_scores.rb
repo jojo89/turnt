3e9f82644e5a303b30b25cb2b00de7275d620d63
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-31 20:09:28 -0500

    post

---
+++ db/migrate/20131101001327_create_scores.rb
@@ -0,0 +1,15 @@
+class CreateScores < ActiveRecord::Migration
+  def change
+    create_table :scores do |t|
+      t.boolean :turn
+      t.integer :user_id
+      t.integer :game_id
+      t.integer :stocks
+      t.integer :bonds
+      t.integer :real_estates
+      t.integer :commodities
+      t.integer :cash
+      t.timestamps
+    end
+  end
+end
